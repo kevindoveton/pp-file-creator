7d3ba9ec70955f634c397e2599a5667b72be871d
kevindoveton <kevindoveton@me.com>
2017-06-04 09:06:44 +0930

    ...

--- src/js/Components/Slide.js
+++ src/js/Components/Slide.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import SlideEditor from './SlideEditor'
 import SlidePreview from './SlidePreview'
@@ -31,3 +32,89 @@ class Slide extends React.Component {
 }
 
 export default Slide;
+
+/*
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { DragSource, DropTarget } from 'react-dnd';
+
+import SlideEditor from './SlideEditor'
+import SlidePreview from './SlidePreview'
+
+
+const cardSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      originalIndex: props.findCard(props.id).index,
+    };
+  },
+
+  endDrag(props, monitor) {
+    const { id: droppedId, originalIndex } = monitor.getItem();
+    const didDrop = monitor.didDrop();
+
+    if (!didDrop) {
+      props.moveCard(droppedId, originalIndex);
+    }
+  },
+};
+
+const cardTarget = {
+  canDrop() {
+    return false;
+  },
+
+  hover(props, monitor) {
+    const { id: draggedId } = monitor.getItem();
+    const { id: overId } = props;
+
+    if (draggedId !== overId) {
+      const { index: overIndex } = props.findCard(overId);
+      props.moveCard(draggedId, overIndex);
+    }
+  },
+};
+
+@DropTarget('slide', cardTarget, connect => ({
+  connectDropTarget: connect.dropTarget(),
+}))
+@DragSource('slide', cardSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging(),
+}))
+
+class Slide extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.slide = this.props.slide;
+    this.id = this.slide.id;
+  }
+  
+  static propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    connectDropTarget: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+    id: PropTypes.any.isRequired,
+    moveCard: PropTypes.func.isRequired,
+    findCard: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { text, isDragging, connectDragSource, connectDropTarget } = this.props;
+    const opacity = isDragging ? 0 : 1;
+
+    return connectDragSource(connectDropTarget(
+      <div>
+        <SlideEditor {...this.props} />
+        <button onMouseDown={this._newSlide}>+</button>
+        <button>-</button>
+        <SlidePreview {...this.props} />
+      </div>
+    ));
+  }
+}
+
+export default Slide;
+*/
--- src/js/Components/SlideList.js
+++ src/js/Components/SlideList.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Slide from '/Components/Slide'
 
@@ -25,3 +26,85 @@ class SlideList extends React.Component {
 }
 
 export default SlideList;
+
+/*
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import update from 'react/lib/update';
+import { DropTarget, DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+import Slide from '/Components/Slide'
+
+const style = {
+  width: 400,
+};
+
+const cardTarget = {
+  drop() {
+  },
+};
+
+@DragDropContext(HTML5Backend)
+@DropTarget('slide', cardTarget, connect => ({
+  connectDropTarget: connect.dropTarget(),
+}))
+
+class SlideList extends Component {
+  static propTypes = {
+    connectDropTarget: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.moveCard = this.moveCard.bind(this);
+    this.findCard = this.findCard.bind(this);
+    this.state = {
+      slides: this.props.slides
+    };
+  }
+  
+  componentDidMount() {
+    this.props.AddSlide();
+  }
+
+  moveCard(id, atIndex) {
+    const { slide, index } = this.findCard(id);
+    
+  }
+
+  findCard(id) {
+    const { slides } = this.state;
+    const slide = slides.filter(slide => {
+      return slide.id === id
+    })[0];
+
+    return {
+      slide,
+      index: slides.valueSeq().indexOf(slide),
+    };
+  }
+
+  render() {
+    const { connectDropTarget } = this.props;
+    const { slides } = this.state;
+
+    return connectDropTarget(
+      <div style={style}>
+        {this.props.slides.valueSeq().map(slide => (
+          <Slide
+            key={slide.id}
+            id={slide.id}
+            slide={slide}
+            moveCard={this.moveCard}
+            findCard={this.findCard}
+            {...this.props}
+          />
+        ))}
+      </div>,
+    );
+  }
+}
+
+export default SlideList;
+*/
