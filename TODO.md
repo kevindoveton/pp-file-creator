ecf7ad6a62b8e3d9794a5022d641038f8f4a981b
kevindoveton <kevindoveton@me.com>
2017-06-16 23:24:03 +0930

    make sure slides get submitted!

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ### TODOs
 | Filename | line # | TODO
 |:------|:------:|:------
-| src/js/controllers/HomeController.ts | 126 | get this dynamically
 | src/js/factories/HttpService.ts | 103 | implement this server side
 | src/js/factories/HttpService.ts | 108 | user storage.. find a better way of reauthorising
+| src/js/controllers/HomeController.ts | 126 | get this dynamically
--- src/js/controllers/HomeController.ts
+++ src/js/controllers/HomeController.ts
@@ -20,7 +20,6 @@ angular.module('ppfilecreator.controllers').controller('HomeCtrl', function($sco
     date: '',
     slides: []
   }
-  $scope.slides = $scope.sermon.slides;
   
   $scope.toolbar = {
     'toolbar': {
@@ -39,6 +38,7 @@ angular.module('ppfilecreator.controllers').controller('HomeCtrl', function($sco
   $scope.slides = [TEXT_SLIDE()];
   
   $scope.submit = function() {
+    $scope.sermon.slides = $scope.slides;
     HttpService.CreateNewDocument($scope.sermon).then(function(d) {
       if (typeof(d.status) !== 'undefined' && d.status == 200) {
         var data = new Blob([d.data], { type: 'text/xml;charset=utf-8' });
