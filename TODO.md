d98e6d1ac652ce926067ba14d459bd435645d150
kevindoveton <kevindoveton@me.com>
2017-06-17 19:01:42 +0930

    add loader between pages

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ### TODOs
 | Filename | line # | TODO
 |:------|:------:|:------
+| src/js/controllers/HomeController.ts | 126 | get this dynamically
 | src/js/factories/HttpService.ts | 107 | implement this server side
 | src/js/factories/HttpService.ts | 112 | user storage.. find a better way of reauthorising
-| src/js/controllers/HomeController.ts | 126 | get this dynamically
--- src/js/controllers.ts
+++ src/js/controllers.ts
@@ -1,7 +1,13 @@
 'use strict';
 /* Controllers */
 angular.module('ppfilecreator.controllers', []).controller('AppCtrl', function ($scope, $rootScope, $http, $templateCache, $state, localStorageService) {
-    
+  
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){ 		
+		$scope.loading = true;
+	})
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){ 
+		$scope.loading = false;
+	});
   // show this normally
   $scope.showHeader = true;
   $scope.showFooter = true;
