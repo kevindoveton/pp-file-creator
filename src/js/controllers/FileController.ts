a7d6224f7a7388e82562717c6aeab799580052fa
kevindoveton <kevindoveton@me.com>
2017-07-17 21:06:55 +0930

    filesList - previews work

--- src/js/controllers/FileController.ts
+++ src/js/controllers/FileController.ts
@@ -6,8 +6,6 @@ angular.module('ppfilecreator.controllers').controller('FileCtrl', function($sco
   });
 
   $scope.download = function(id) {
-    // var blob = new Blob([$scope.presentations[index].slide], {type: 'text/xml;charset=utf-8'});
-    // FileSaver.saveAs(blob, $scope.presentations[index].title + '.pro5');
     $window.location = '__API-URL__/api/v1/files/'+id
   }
 
