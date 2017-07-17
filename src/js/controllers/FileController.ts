4d71b6b533462876b4ff80363209361b3af84f49
kevindoveton <kevindoveton@me.com>
2017-07-18 07:39:36 +0930

    show file preview correctly

--- src/js/controllers/FileController.ts
+++ src/js/controllers/FileController.ts
@@ -2,11 +2,29 @@ angular.module('ppfilecreator.controllers').controller('FileCtrl', function($sco
   $scope.presentations = [];
   HttpService.GetDocuments().then(function(success) {
     $scope.presentations = success.data
-    console.log(success.data);
+    
+    HttpService.GetTemplates().then(function(success) {
+      reloadTemplates(success.data);
+    });
+    
   });
+  
+  
 
   $scope.download = function(id) {
     $window.location = '__API-URL__/api/v1/files/'+id
+  }
+
+  function reloadTemplates(templates) {
+    for (var i = 0; i < templates.length; i++) {
+      for (var j = 0; j < $scope.presentations.length; j++) {
+        if (templates[i]['_id'] == $scope.presentations[j]['template']) {
+          $scope.presentations[j]['template'] = templates[i];
+          
+        }
+        console.log(templates[i]['_id'] == $scope.presentations[j]['template'])
+      }
+    }
   }
 
 });
