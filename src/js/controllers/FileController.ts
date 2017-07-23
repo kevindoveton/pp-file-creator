b6232fff8f6833130ee735fed5bac58fee19b856
kevindoveton <kevindoveton@me.com>
2017-07-23 17:52:30 +0930

    fix - download files

--- src/js/controllers/FileController.ts
+++ src/js/controllers/FileController.ts
@@ -1,4 +1,4 @@
-angular.module('ppfilecreator.controllers').controller('FileCtrl', function($scope, ModalService, HttpService, $window) {
+angular.module('ppfilecreator.controllers').controller('FileCtrl', function($scope, ModalService, HttpService, $window, localStorageService) {
   $scope.presentations = [];
   HttpService.GetDocuments().then(function(success) {
     $scope.presentations = success.data
@@ -9,7 +9,7 @@ angular.module('ppfilecreator.controllers').controller('FileCtrl', function($sco
   });
 
   $scope.download = function(id) {
-    $window.location = '__API-URL__/api/v1/files/'+id
+    $window.location = '__API-URL__/api/v1/files/'+id+'?token='+localStorageService.get('accessToken')
   }
 
   function reloadTemplates(templates) {
